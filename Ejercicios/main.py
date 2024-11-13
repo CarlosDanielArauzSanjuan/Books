0a3ed1414b4eaa8b0c74a3540c37599bdeacad3d
CarlosDanielArauzSanjuan <186082333+CarlosDanielArauzSanjuan@users.noreply.github.com>
2024-11-12 23:45:10 -0500

    Exercise9

--- Ejercicios/main.py
+++ Ejercicios/main.py
@@ -1,9 +1,11 @@
+# Ejercicio 9 - Clasificacón de edades
 
-
-# Ejercicio 8 - Determinar año bisiesto
-
-año = int(input("Ingrese un año: "))
-if año % 4 == 0 and año % 100 != 0 or año % 400 == 0:
-  print("El año es bisiesto")
-else:
-  print("El año no es bisiesto")
+edad = int(input("Ingrese una edad: "))
+if edad >= 0 and edad <=12:
+  print("La persona es un niño")
+elif edad >= 13 and edad <= 17:
+  print("La persona es un adolescente")
+elif edad >= 18 and edad <= 64:
+  print("La persona es un adulto")
+else: 
+  print("La persona es un anciano")
