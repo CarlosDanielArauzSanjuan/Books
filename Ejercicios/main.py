4be141614999a1ab7a493b207b3e6322a66a82d2
CarlosDanielArauzSanjuan <186082333+CarlosDanielArauzSanjuan@users.noreply.github.com>
2024-11-12 23:50:52 -0500

    Exercise14

--- Ejercicios/main.py
+++ Ejercicios/main.py
@@ -1,12 +1,35 @@
-# Ejercicio 13 - Comparación de 3 números
+# Ejercicio 14 - Adivinanza de letras
 
-num1 = int(input("Ingrese un numero: "))
-num2 = int(input("Ingrese otro numero: "))
-num3 = int(input("Ingrese otro numero: "))
+letra = input("Adivina la letra: ")
+secreta = "f"
 
-if num1 > num2 and num1 > num3:
-  print("El numero", num1, "es el mayor")
-elif num2 > num1 and num2 > num3:
-  print("El numero", num2, "es el mayor")
-else:
-  print("El numero", num3, "es el mayor")
+def adivinarLetra(letra):
+  match letra:
+    case "a": return "Letra incorrecta"
+    case "b": return "Letra incorrecta"
+    case "c": return "Letra incorrecta"
+    case "d": return "Letra incorrecta"
+    case "e": return "Letra incorrecta"
+    case "f": return "Adivinaste"
+    case "g": return "Letra incorrecta"
+    case "h": return "Letra incorrecta"
+    case "i": return "Letra incorrecta"
+    case "j": return "Letra incorrecta"
+    case "k": return "Letra incorrecta"
+    case "l": return "Letra incorrecta"
+    case "m": return "Letra incorrecta"
+    case "n": return "Letra incorrecta"
+    case "o": return "Letra incorrecta"
+    case "p": return "Letra incorrecta"
+    case "q": return "Letra incorrecta"
+    case "r": return "Letra incorrecta"
+    case "s": return "Letra incorrecta"
+    case "t": return "Letra incorrecta"
+    case "u": return "Letra incorrecta"
+    case "v": return "Letra incorrecta"
+    case "w": return "Letra incorrecta"
+    case "x": return "Letra incorrecta"
+    case "y": return "Letra incorrecta"
+    case "z": return "Letra incorrecta"
+
+print(adivinarLetra(letra), "la letra era:", secreta)
