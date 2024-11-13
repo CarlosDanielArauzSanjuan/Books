9ad6b027c1d9cc9230be9913fa6e49b24716ca11
CarlosDanielArauzSanjuan <186082333+CarlosDanielArauzSanjuan@users.noreply.github.com>
2024-11-12 23:57:28 -0500

    ivesStructures4

--- Ejercicios/main.py
+++ Ejercicios/main.py
@@ -1,8 +1,7 @@
 #IterativesStructures
-# Ejercicio 3 - Factorial de un número
-n = int(input("Ingrese un número entero positivo: "))
-factorial = 1
-for i in range(1, n + 1):
-  factorial *= i
-print("El factorial de", n, "es:", factorial)
-
+# Ejercicio 4 - Pares en un rango
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+for i in range(num1, num2 + 1):
+  if i % 2 == 0:
+    print(i)
