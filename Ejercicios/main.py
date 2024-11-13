d4b82ca281fe13f04e601fb629722328b3611f06
CarlosDanielArauzSanjuan <186082333+CarlosDanielArauzSanjuan@users.noreply.github.com>
2024-11-12 23:51:50 -0500

    Exercise16

--- Ejercicios/main.py
+++ Ejercicios/main.py
@@ -1,17 +1,11 @@
-# Ejercicio 15 - Cálculo del salario neto
+# Ejercicio 16 - Cálculo del tiempo de viaje
 
-sBruto = int(input("Ingrese su salario bruto: "))
-pais = int(input("Ingrese su pais de residencia (1 -> Pais A, 2 -> Pais B, 3 -> Pais C, 4 -> Otro): "))
-
-def calculadoraSalario(sBruto, pais):
-  match pais:
-    case 1:
-      return sBruto - (sBruto * 0.2)
-    case 2:
-      return sBruto - (sBruto * 0.15)
-    case 3:
-      return sBruto - (sBruto * 0.1)
-    case 4:
-      return sBruto - (sBruto * 0.25)
-
-print("El salario Neto es:", calculadoraSalario(sBruto, pais), "$")
+import math
+distancia = float(input("Ingrese la distancia en km: "))
+velocidad = float(input("Ingrese la velocidad en km/h: "))
+tiempo = distancia / velocidad
+decimal, entero = math.modf(tiempo)
+tiempoMinutos = decimal * 60
+print("El tiempo de viaje es:", entero, "horas y", round(tiempoMinutos), "minutos")
+if velocidad > 120:
+  print("Advertencia, no exceda los limites de velocidad")
