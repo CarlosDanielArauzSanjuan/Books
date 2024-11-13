fe49752544165e41df44992ddb18b229044c3e5c
CarlosDanielArauzSanjuan <186082333+CarlosDanielArauzSanjuan@users.noreply.github.com>
2024-11-12 23:55:35 -0500

    IterativesStructures1

--- Ejercicios/main.py
+++ Ejercicios/main.py
@@ -1,12 +1,6 @@
-# Ejercicio 22 - Clasificación de triángulos por ángulos
-
-angulo1 = float(input("Ingrese el valor del ángulo 1: "))
-angulo2 = float(input("Ingrese el valor del ángulo 2: "))
-angulo3 = float(input("Ingrese el valor del ángulo 3: "))
-
-if angulo1 < 90 and angulo2 < 90 and angulo3 < 90:
-  print("El triángulo es agudo")
-elif angulo1 == 90 or angulo2 == 90 or angulo3 == 90:
-  print("El triángulo es rectángulo")
-elif angulo1 > 90 or angulo2 > 90 or angulo3 > 90:
-  print("El triángulo es obtuso")
+# Ejercicio 1 - Suma de los primeros n números IterativesStructures
+n = int(input("Ingrese un número positivo: "))
+suma = 0
+for i in range(n + 1):
+  suma += i
+print("La suma de los primeros", n, "números es:", suma)
